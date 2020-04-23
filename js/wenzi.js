c41c3006e7015f56d429f7802dc0ad3b4663ceec
hpb1-boke <742921191@qq.com>
2020-04-23 23:58:49 +0800

    Site updated: 2020-04-23 23:58:19

--- js/wenzi.js
+++ js/wenzi.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 23 2020 23:24:44 GMT+0800 (GMT+08:00)
+// build time:Thu Apr 23 2020 23:56:57 GMT+0800 (GMT+08:00)
 var a_idx=0;jQuery(document).ready(function(a){a("body").click(function(o){var n=new Array("å¯Œå¼º","æ°‘ä¸»","æ–‡æ˜Ž","å’Œè°","è‡ªç”±","å¹³ç­‰","å…¬æ­£","æ³•æ²»","çˆ±å›½","æ•¬ä¸š","è¯šä¿¡","å‹å–„");var t=a("<span></span>").text(n[a_idx]);a_idx=(a_idx+1)%n.length;var e=o.pageX,d=o.pageY;t.css({"z-index":1e69,top:d-20,left:e,position:"absolute","font-weight":"bold",color:"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"});a("body").append(t);t.animate({top:d-180,opacity:0},1500,function(){t.remove()})})});
 //rebuild by neat 
--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 23 2020 23:24:44 GMT+0800 (GMT+08:00)
+// build time:Thu Apr 23 2020 23:56:57 GMT+0800 (GMT+08:00)
 $(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
 //rebuild by neat 

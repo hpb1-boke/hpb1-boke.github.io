28a39371faca657525d274bd7010856a0b2afbbb
hpb1-boke <742921191@qq.com>
2020-04-30 15:59:41 +0800

    Site updated: 2020-04-30 15:59:02

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,3 +1,3 @@
-// build time:Sat Apr 25 2020 21:10:33 GMT+0800 (GMT+08:00)
+// build time:Thu Apr 30 2020 15:58:28 GMT+0800 (GMT+08:00)
 $(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
 //rebuild by neat 
